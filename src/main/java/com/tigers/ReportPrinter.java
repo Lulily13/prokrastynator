a7826ff75e8575819fd4a51605b0ba711ff00d84
ElMihalk <mikostrz@d17-427-pc-09.ipa.ii.agh.edu.pl>
2025-06-07 12:17:39 +0200

    Interfaces update

--- src/main/java/com/tigers/ReportPrinter.java
+++ src/main/java/com/tigers/ReportPrinter.java
@@ -1,4 +1,7 @@
 package com.tigers;
 
+import java.util.Collection;
+
 public interface ReportPrinter {
+    public void printReport(Collection<String> report);
 }
--- src/main/java/com/tigers/Segregator.java
+++ src/main/java/com/tigers/Segregator.java
@@ -1,4 +1,7 @@
 package com.tigers;
 
+import java.util.Collection;
+
 public interface Segregator {
+    public Collection<String> prepareReport(DataCollector dataCollector);
 }
