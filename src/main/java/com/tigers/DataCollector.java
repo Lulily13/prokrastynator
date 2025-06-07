79a6a9ace16577dae67990107c2284aec87e9cdf
ElMihalk <mikostrz@d17-427-pc-09.ipa.ii.agh.edu.pl>
2025-06-07 12:30:02 +0200

    DataCollector update

--- src/main/java/com/tigers/DataCollector.java
+++ src/main/java/com/tigers/DataCollector.java
@@ -1,4 +1,17 @@
 package com.tigers;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class DataCollector {
+    public Collection<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Collection<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    private Collection<Task> tasks = new ArrayList<>();
+
 }
